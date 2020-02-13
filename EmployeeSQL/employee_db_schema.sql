1c1280b99347814409f9bac46833bb7f67268096
Atul Nulkar <nulkar@gmail.com>
2020-02-13 00:47:56 -0500

    SQL script to create tables and import CSVs to employee_db

--- EmployeeSQL/employee_db_schema.sql
+++ EmployeeSQL/employee_db_schema.sql
@@ -1,2 +1,118 @@
--- Create a table schema for each of the six CSV files. 
--- Remember to specify data types, primary keys, foreign keys, and other constraints.
+-- This SQL script creates a table schema for each of the six CSV files provided for the Employee Database.
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/YF3XjC
+
+CREATE TABLE "departments" (
+    "dept_no" CHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(100)   NOT NULL,
+    "last_name" VARCHAR(100)   NOT NULL,
+    "gender" CHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "emp_no" INTEGER   NOT NULL,
+    "title" VARCHAR(100)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "emp_no","title","from_date"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" CHAR(4)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" CHAR(4)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+COPY departments(dept_no, dept_name) 
+FROM '/Users/Shared/Resources/departments.csv'
+DELIMITER ',' CSV HEADER;
+
+select * from departments;
+
+COPY employees(emp_no, birth_date, first_name, last_name, gender, hire_date) 
+FROM '/Users/Shared/Resources/employees.csv'
+DELIMITER ',' CSV HEADER;
+
+select * from employees;
+
+COPY salaries(emp_no, salary, from_date, to_date) 
+FROM '/Users/Shared/Resources/salaries.csv'
+DELIMITER ',' CSV HEADER;
+
+select * from salaries;
+
+COPY titles(emp_no, title, from_date, to_date) 
+FROM '/Users/Shared/Resources/titles.csv'
+DELIMITER ',' CSV HEADER;
+
+select * from titles;
+
+COPY dept_manager(dept_no, emp_no, from_date, to_date) 
+FROM '/Users/Shared/Resources/dept_manager.csv'
+DELIMITER ',' CSV HEADER;
+
+select * from dept_manager;
+
+COPY dept_emp(emp_no, dept_no, from_date, to_date) 
+FROM '/Users/Shared/Resources/dept_emp.csv'
+DELIMITER ',' CSV HEADER;
+
+select * from dept_emp;
+
