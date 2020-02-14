4928bc254eb7e00004821e6272c47f6b51c8d4e2
Atul Nulkar <nulkar@gmail.com>
2020-02-14 00:22:27 -0500

    Updated SQL script with initial Data Analysis

--- EmployeeSQL/employee_db_queries.sql
+++ EmployeeSQL/employee_db_queries.sql
@@ -1,17 +1,34 @@
+/*
+-----------------------------------------------------------------------------
+This SQL script contains queuries to perform Data Analysis on the Employee DB
+-----------------------------------------------------------------------------
+*/
+
 -- List the following details of each employee: 
 -- employee number, last name, first name, gender, and salary.
-
+SELECT emp.emp_no, emp.first_name, emp.last_name, emp.gender, sal.salary
+FROM employees AS emp
+JOIN salaries AS sal ON emp.emp_no = sal.emp_no
+ORDER BY emp_no;
 
 -- List employees who were hired in 1986.
-
+SELECT * FROM employees
+WHERE EXTRACT(YEAR FROM employees.hire_date)=1986;
 
 -- List the manager of each department with the following information: 
 -- department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
-
+SELECT d.dept_no, d.dept_name, dm.emp_no, emp.first_name, emp.last_name, dm.from_date, dm.to_date
+FROM departments AS d
+JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
+JOIN employees AS emp ON dm.emp_no = emp.emp_no;
 
 -- List the department of each employee with the following information: 
 -- employee number, last name, first name, and department name.
-
+SELECT emp.emp_no, emp.first_name, emp.last_name, dept.dept_name
+FROM employees AS emp
+JOIN dept_emp AS demp ON emp.emp_no = demp.emp_no
+JOIN departments AS dept ON demp.dept_no = dept.dept_no
+ORDER BY emp_no;
 
 -- List all employees whose first name is "Hercules" and last names begin with "B."
 
